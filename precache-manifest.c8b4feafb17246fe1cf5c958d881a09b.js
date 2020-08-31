0b39993968886c4252843f9bd8e9728db866b7a9
alexvirga <alexander.virga@gmail.com>
2020-08-31 19:50:24 -0400

    Updates

---
+++ precache-manifest.c8b4feafb17246fe1cf5c958d881a09b.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "83c983545e524a98051aa1224abf3a4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e50d242dfa28995f4c45",
+    "url": "/static/css/main.e0d931b9.chunk.css"
+  },
+  {
+    "revision": "f8753fc8380af5ba6970",
+    "url": "/static/js/2.f39abafd.chunk.js"
+  },
+  {
+    "revision": "9a9e56582fbb6743dfc4c1baab3b2d6f",
+    "url": "/static/js/2.f39abafd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e50d242dfa28995f4c45",
+    "url": "/static/js/main.0a9b64a3.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "72536fcf628cdebf82c09d72b5c86ad0",
+    "url": "/static/media/alexplanet.72536fcf.png"
+  },
+  {
+    "revision": "d992fa20e46c919bf199d473e5ff7b39",
+    "url": "/static/media/artwork.d992fa20.jpg"
+  },
+  {
+    "revision": "faa0100cb0bfbd61a4d1c75b4b5278b1",
+    "url": "/static/media/bantergif.faa0100c.gif"
+  },
+  {
+    "revision": "1621667950b8602413cc0dfba34df1b6",
+    "url": "/static/media/circle-cropped.16216679.png"
+  },
+  {
+    "revision": "65478f20b9d1be28f28a98b621073a62",
+    "url": "/static/media/colorschemes.65478f20.jpg"
+  },
+  {
+    "revision": "606c5ccbd3da3e234cdc2a16766f14d5",
+    "url": "/static/media/exempla-assignment.606c5ccb.jpg"
+  },
+  {
+    "revision": "3070f62ca516782e0d9af4ef2a46b390",
+    "url": "/static/media/exempla-dashboard.3070f62c.jpg"
+  },
+  {
+    "revision": "533fb4b2d683f21a90bac5759a322401",
+    "url": "/static/media/exempla-events.533fb4b2.jpg"
+  },
+  {
+    "revision": "4ab928087221307f343bc30d18c04c8d",
+    "url": "/static/media/exempla-gif.4ab92808.gif"
+  },
+  {
+    "revision": "018a588894c2c9bb27c3060ce0d255d3",
+    "url": "/static/media/exempla-gif2.018a5888.gif"
+  },
+  {
+    "revision": "f5ab63a4be198a7c194518b59f1cbe36",
+    "url": "/static/media/exempla-profile.f5ab63a4.png"
+  },
+  {
+    "revision": "9b7138182a659370613ae7e146ea43d4",
+    "url": "/static/media/maphunt.9b713818.jpg"
+  },
+  {
+    "revision": "3a52b0f2441fd29672a61b0c0fa1bd48",
+    "url": "/static/media/ourmaskgif.3a52b0f2.gif"
+  },
+  {
+    "revision": "213c1b25664451067fb5ef1f5ee85b29",
+    "url": "/static/media/pipeline.213c1b25.jpg"
+  }
+]);
